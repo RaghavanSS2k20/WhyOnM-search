6f60e1e259599327a2073ede191a5c3d81710f75
raghavan <rn75creatography@gmail.com>
2024-04-15 00:16:53 +0530

    ok but errorouneous

--- controller.py
+++ controller.py
@@ -35,6 +35,7 @@ def getRanking(allPosts, query):
     print(len(tdm.columns))
     idf = np.log((N + 1) / (df + 0.5))
     tdm = tdm.mul(idf, axis=1)
+    print(tdm)
     processedQuery = [query_processing(query, unique_terms)]
     qvector = pd.DataFrame(processedQuery)
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++===================================================++++++++++++++++++++++++++++++++++++", qvector)
@@ -55,6 +56,116 @@ def getRanking(allPosts, query):
 
 
             
+def getRankingWithTitle(allPosts, query):
+    corpus = [tokenize(post['content']) for post in allPosts["posts"]]
+    titleCorpus = [tokenize(post['title']) for post in allPosts["posts"]]
+    post_ids  = [post["_id"] for post in allPosts["posts"]]
+    N = len(corpus)
+    ByTitleN = len(titleCorpus)
+    cnto = 1
+    for i in titleCorpus:
+        print(i, cnto)
+        cnto= cnto+1
+    sw = current_app.stopwords
+    # print(sw)
+    for i in range(len(corpus)):
+        corpus[i] = [term for term in corpus[i] if term not in sw]
+    for i in range(len(titleCorpus)):
+        titleCorpus[i] = [term for term in titleCorpus[i]]
+    cnto = 1
+    for i in titleCorpus:
+        print(i, cnto)
+        cnto= cnto+1
+    unique_terms = set()
+    unique_title_terms = set()
+
+    for document in corpus:
+        unique_terms = unique_terms.union(set(document))
+    rows = []
+    rows_for_title = []
+    for title in titleCorpus:
+        unique_title_terms = unique_title_terms.union(set(title))
+
+
+    for document in corpus:
+        freq_vector = dict.fromkeys(unique_terms, 0)
+        for term in document:
+            freq_vector[term] += 1
+        rows.append(freq_vector)
+    
+    for title in titleCorpus:
+        freq_vector_title = dict.fromkeys(unique_title_terms, 0)
+        for term in title:
+            freq_vector_title[term] += 1
+        rows_for_title.append(freq_vector_title)
+    # print(rows)
+    # print("ROWWWWWWWWWWWWWWWWSSSSSSSS",rows_for_title)
+    tdm = pd.DataFrame(rows)
+    title_tdm = pd.DataFrame(rows_for_title)
+    
+    rows_with_all_zeroes = (title_tdm == 0).all(axis=1)
+
+# Print rows with all zeroes
+    for i in  unique_title_terms:
+        print(i)
+    print("Rows with all zeroes:")
+    print(title_tdm[rows_with_all_zeroes])
+    tdm = tdm.div(tdm.sum(axis=1), axis=0)
+    title_tdm = title_tdm.div(title_tdm.sum(axis=1), axis=0)
+    print(title_tdm)
+    df = tdm.astype(bool).sum(axis=0)
+    tdf = title_tdm.astype(bool).sum(axis=0)
+    print(len(tdm.columns))
+    for i in range(len(tdf)):
+        print(tdf.iloc[i])
+    idf = np.log((N + 1) / (df + 0.5))
+    title_idf = np.log((ByTitleN + 1) / (tdf + 0.5))
+    tdm = tdm.mul(idf, axis=1)
+    print(title_tdm)
+    title_tdm = title_tdm.mul(title_idf, axis=1)
+    print(tdm)
+    print(title_tdm)
+    processedQuery = [query_processing(query, unique_terms)]
+    processedQueryForTitle = [query_processing(query, unique_title_terms)]
+    qvector = pd.DataFrame(processedQuery)
+    titleqvector = pd.DataFrame(processedQueryForTitle)
+    print("uifhijsadbvjewqhfasdbvjieqrbfgjqwebnijq3rfkqsdnfug",titleqvector)
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++===================================================++++++++++++++++++++++++++++++++++++", qvector)
+    qvector = qvector.div(qvector.sum(axis=1), axis=0)
+    qvector = qvector.mul(idf, axis=1)
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++===================================================++++++++++++++++++++++++++++++++++++", titleqvector)
+    titleqvector = titleqvector.div(titleqvector.sum(axis=1), axis=0)
+    titleqvector = titleqvector.mul(title_idf, axis=1)
+    lst = []
+    title_lst = []
+    for q in range(len(qvector)):
+        for i in range(len(tdm)):
+            lst.append(find_cosine_sim(tdm.iloc[i], qvector.iloc[q]))
+    for q in range(len(titleqvector)):
+        for i in range(len(title_tdm)):
+            title_lst.append(find_cosine_sim(title_tdm.iloc[i], titleqvector.iloc[q]))
+    
+    # similarity_df = pd.DataFrame(lst, columns=['post_id', 'similarity'])
+    # print(similarity_df)
+    # similarity_df = similarity_df.sort_values(by='similarity', ascending=False)
+    # similarity_json = similarity_df.to_json(orient='records')
+    # pnt = 0
+    # for ls in title_lst:
+    #     pnt = pnt+1
+    #     print(ls, pnt)
+    alpha = 0.7  # Set your alpha value
+
+    result_lst = []
+
+    for i in range(len(lst)):
+        result = alpha * title_lst[i] + (1 - alpha) * lst[i]
+        result_lst.append([post_ids[i],result])
+    print(result_lst)
+    similarity_df = pd.DataFrame(result_lst, columns=['post_id', 'similarity'])
+    print(similarity_df)
+    similarity_df = similarity_df.sort_values(by='similarity', ascending=False)
+    similarity_json = similarity_df.to_json(orient='records')
+    return similarity_json
 
 
 
--- search-app.py
+++ search-app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, current_app, request
-from controller import getRanking
+from controller import getRanking, getRankingWithTitle
 from helpers import getAllPosts
 app = Flask(__name__)
 import os
@@ -9,14 +9,14 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 from controller import some_function
 load_dotenv()
-# print(os.getenv("TEST"))
+print(os.getenv("TESTE"))
 
-cluster = MongoClient(os.getenv("DB_URL"))
+# cluster = MongoClient(os.getenv("DB_URL"))
 with app.app_context():
     nltk.download('stopwords')
     current_app.stopwords = set(nltk.corpus.stopwords.words('english'))
 
-@app.route('/')
+@app.route('/',)
 def hello_world():
     some_function()
     return jsonify({"message":200})
@@ -27,12 +27,16 @@ def search():
     data = request.json
     term = data["term"]
     allPosts = getAllPosts()
-    rankedPosts = getRanking(allPosts, term)
+    rankedPosts = getRankingWithTitle(allPosts, term)
     return rankedPosts, 200
 
     
+@app.route('/job', methods=['POST'])
+def pull():
+    pass
 
-    val = getRanking()
+
+    
 if __name__ == "__main__":
 
     app.run(debug = True)
